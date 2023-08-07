1c4c9bac962b5cbadbad7db6e8a0763d58e064f5
user <mehulprajapati1661@gmail.com>
2023-08-07 22:32:49 +0530

    design completed

--- app/src/main/java/com/example/mad_practical4_21012011118/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical4_21012011118/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mad_practical4_21012011118
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val signUpTxt: TextView = findViewById(R.id.signUpText)
+
+        signUpTxt.setOnClickListener {
+            Intent(this@LoginActivity, RegistrationActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mad_practical4_21012011118/RegistrationActivity.kt
+++ app/src/main/java/com/example/mad_practical4_21012011118/RegistrationActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mad_practical4_21012011118
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        val loginTxt:TextView = findViewById(R.id.loginText)
+
+        loginTxt.setOnClickListener {
+            Intent(this@RegistrationActivity,LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
