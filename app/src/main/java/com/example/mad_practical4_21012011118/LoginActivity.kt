9d9b93d965d95250b8aba83d102b6ee6a74cdb25
student <student@gnu.ac.in>
2023-08-08 10:21:22 +0530

    practical 4 completed

--- app/src/main/java/com/example/mad_practical4_21012011118/LoginActivity.kt
+++ app/src/main/java/com/example/mad_practical4_21012011118/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.mad_practical4_21012011118
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,16 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val signUpTxt: TextView = findViewById(R.id.signUpText)
+        val loginButton: Button = findViewById(R.id.loginButton)
 
         signUpTxt.setOnClickListener {
             Intent(this@LoginActivity, RegistrationActivity::class.java).also {
                 startActivity(it)
             }
+        }
+
+        loginButton.setOnClickListener {
+            Toast.makeText(this@LoginActivity, "Login Successfully", Toast.LENGTH_LONG).show()
         }
     }
 }
